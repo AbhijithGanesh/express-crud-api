24f39848dc6fb2eed06161d8c80cd02612c61896
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-05 00:12:56 +0530

    Workflows (#2)
    * Addition of linter
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Addition of Prettify, ES2020
    
    * Addition of TSLint.JSON
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Removal of linting
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Prettify changes
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Allows console logs
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 const app: any = express();
 const port: number = 3000;
-
 
 app.listen(port, () => {
   console.log(`The server is sucessfully running in port ${port}`);
 });
 
-
-
-app.get("/", (req: Request, res:  Response) => {
-    res.send("Hello World")
-
-})
+app.get("/:name/", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.send("Hello World");
+});
