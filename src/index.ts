bcf135e7c0e06175a7b264c57df9e93e931e8d20
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-05 15:18:09 +0530

    Addition of TS post lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
-import { IssueRouter } from "./routes/issues";
-import path from "path";
+import express, { Request, Response } from 'express';
+import { IssueRouter } from './routes/issues';
+import path from 'path';
 
 const app: any = express();
 const port: number = 3000;
@@ -9,10 +9,10 @@ app.listen(port, () => {
   console.log(`The server is sucessfully running in port ${port}`);
 });
 
-app.use("/static", express.static(path.join(__dirname, "public")));
-app.use("/issues", IssueRouter);
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/issues', IssueRouter);
 
-app.get("/:name/", (req: Request, res: Response) => {
+app.get('/:name/', (req: Request, res: Response) => {
   console.log(req.params);
-  res.send("Hello World");
+  res.send('Hello World');
 });
--- src/routes/issues.ts
+++ src/routes/issues.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
 const IssueRouter: Router = Router();
 
-IssueRouter.get("/welcome", (req: Request, res: Response) => {
-  res.send("Hello World!");
+IssueRouter.get('/welcome', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export { IssueRouter };
