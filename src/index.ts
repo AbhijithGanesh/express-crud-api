ac8da40edd2f9187367b9c34a6ea4b5c91b61839
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-05 15:02:37 +0530

    Post lint effects
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import { IssueRouter } from "./routes/issues";
+import path from "path";
 
 const app: any = express();
 const port: number = 3000;
@@ -6,6 +8,9 @@ const port: number = 3000;
 app.listen(port, () => {
   console.log(`The server is sucessfully running in port ${port}`);
 });
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/issues", IssueRouter);
 
 app.get("/:name/", (req: Request, res: Response) => {
   console.log(req.params);
---
+++ src/routes/issues.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const IssueRouter: Router = Router();
+
+IssueRouter.get("/welcome", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export { IssueRouter };
