66ae9505d6a86eef79cf6c351be911bc4d35e9b1
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-03-06 18:29:45 +0530

    Addition of models and prisma connection method
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

---
+++ prisma/conn.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+}
+
+main()
+  .catch((err) => {
+    throw err;
+  })
+
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+export { prisma };
